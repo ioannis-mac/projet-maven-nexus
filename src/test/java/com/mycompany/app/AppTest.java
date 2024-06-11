10fe01c11e23d66385ac847009d16184aff3f8cc
Aymen Drira <driraaymen@gmail.com>
2024-06-11 16:11:55 +0200

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -27,7 +27,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue2()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
         @Test
     public void shouldAnswerWithTrue3()
